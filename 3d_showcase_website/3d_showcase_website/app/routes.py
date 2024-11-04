bc51989f986a686dd74d941ac2f6862d0e14b06a
elvispangar <114806964+elvispangar@users.noreply.github.com>
2024-11-04 01:57:23 +0100

    Delete 3d_showcase_website directory

--- 3d_showcase_website/3d_showcase_website/app/routes.py
+++
@@ -1,8 +0,0 @@
-# app/routes.py
-from flask import render_template, Blueprint
-
-main = Blueprint('main', __name__)
-
-@main.route('/')
-def index():
-    return render_template('index.html')
--- 3d_showcase_website/3d_showcase_website/config.py
+++
@@ -1,5 +0,0 @@
-# config.py
-class Config:
-    SECRET_KEY = 'your-secret-key-here'
-    TEMPLATES_AUTO_RELOAD = True
-    
